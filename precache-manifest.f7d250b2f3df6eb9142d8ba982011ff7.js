4fc3b359684966e74c142f403ac75dc0dbfe5d57
oderlive <art20012004@gmail.com>
2022-10-31 19:18:00 +0300

    Updates

---
+++ precache-manifest.f7d250b2f3df6eb9142d8ba982011ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04ccffc07b00882f2d7",
+    "url": "/oderlive/reactCounter.git/static/css/main.8b9204a3.chunk.css"
+  },
+  {
+    "revision": "c04ccffc07b00882f2d7",
+    "url": "/oderlive/reactCounter.git/static/js/main.c04ccffc.chunk.js"
+  },
+  {
+    "revision": "67911696f9bf7b2e8777",
+    "url": "/oderlive/reactCounter.git/static/js/1.67911696.chunk.js"
+  },
+  {
+    "revision": "b1560a1a3fd7f13d044f",
+    "url": "/oderlive/reactCounter.git/static/js/runtime~main.b1560a1a.js"
+  },
+  {
+    "revision": "f72b3b86dc2fb1abba1f732d5df4db31",
+    "url": "/oderlive/reactCounter.git/index.html"
+  }
+];
